dfacf7803fb83ab3af902f172c23136c34eb33e4
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-21 05:03:13 +0530

    this is my generater gif 3 commit

--- src/components/Random.js
+++ src/components/Random.js
@@ -6,8 +6,8 @@ const Random = () => {
 
     }
   return (
-    <div classname="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center">
-      <h1>Random Gif</h1>
+    <div classname="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+      <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
       <img src={gif} width="450"/>
       <button onClick={clickHandler}>Generate</button>
     </div>
