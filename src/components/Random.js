bdb12214d1b990b200033183b58b15e014d49129
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-29 19:15:41 +0530

    this is my random generator 4 commit

--- src/components/Random.js
+++ src/components/Random.js
@@ -9,7 +9,8 @@ const Random = () => {
     <div classname="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
       
-      <img src={gif} width="450"/>
+      
+      <img src={gif} width="450"alt="a gif"/>
       <button onClick={clickHandler} classname="w-10/12 bg-yellow-500 text-lg py-5 py-2 rounded-lg  ">Generate</button>
     </div>
   )
