5dc3a765ebbc099622863bdff40c9f3bf55d7e70
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-29 18:49:16 +0530

    this is my git converter 1 commit

--- src/components/Random.js
+++ src/components/Random.js
@@ -8,8 +8,9 @@ const Random = () => {
   return (
     <div classname="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
+      
       <img src={gif} width="450"/>
-      <button onClick={clickHandler}>Generate</button>
+      <button onClick={clickHandler} classname="w-10/12 bg-yellow-500 text-lg py-5 py-2 rounded-lg  ">Generate</button>
     </div>
   )
 }
